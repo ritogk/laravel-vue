f851836fe109b2b2c8762ce7b81a96132ad32328
root <homing-job@yahoo.co.jp>
2022-01-29 11:42:02 +0900

    会員一覧取得api作成

--- project/app/Http/Controllers/Api/Master/UserController.php
+++ project/app/Http/Controllers/Api/Master/UserController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers\Api\Master;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+// request
 use App\Http\Requests\Master\UserListRequest;
-
 // usecase
 use App\UseCases\Master\User\ListAction;
+// openapi
+use App\OpenAPI;
+use App\Libs\OpenAPIUtility;
 
 class UserController extends Controller
 {
@@ -15,10 +21,20 @@ class UserController extends Controller
      *
      * @param  UserListRequest $request
      * @param  ListAction $action
-     * @return array
+     * @return Response
      */
-    public function list(UserListRequest $request, ListAction $action): array
+    public function list(UserListRequest $request, ListAction $action): Response
     {
-        return $action($request->filter, $request->fields);
+        $parameters = new OpenAPI\Model\QueryUserList($request->all());
+        $result = $action(
+            $parameters->getName(),
+            $parameters->getEmail(),
+            $parameters->getSelfPr(),
+            $parameters->getTel()
+        );
+        return response()->json(
+            OpenAPIUtility::dicstionariesToModelContainers(OpenAPI\Model\User::class, $result),
+            Response::HTTP_OK
+        );
     }
 }
--- project/app/UseCases/Master/User/ListAction.php
+++ project/app/UseCases/Master/User/ListAction.php
@@ -4,22 +4,28 @@ namespace App\UseCases\Master\User;
 
 use App\Models\User;
 
-class ListAction{
+class ListAction
+{
     /**
-     * __invoke
+     * Undocumented function
      *
-     * @param string $filter
-     * @param string $fileds
+     * @param string|null $name
+     * @param string|null $email
+     * @param string|null $self_pr
+     * @param string|null $tel
      * @return array
      */
-    public function __invoke(string $filter, string $fileds): array
+    public function __invoke(string $name = null, string $email = null, string $self_pr = null, string $tel = null): array
     {
-        $items = User::where(json_decode($filter))
-                    ->select(json_decode($fileds))
-                    ->orderBy('id')
-                    ->get()
-                    ->toArray();
-
+        $items = User::when(isset($name), function ($query) use ($name) {
+            return $query->where('name', 'like', "%$name%");
+        })->when(isset($email), function ($query) use ($email) {
+            return $query->where('email', 'like', "%$email%");
+        })->when(isset($self_pr), function ($query) use ($self_pr) {
+            return $query->where('self_pr', 'like', "%$self_pr%");
+        })->when(isset($tel), function ($query) use ($tel) {
+            return $query->where('tel', 'like', "%$tel%");
+        })->orderBy('id')->get()->toArray();
         return $items;
     }
 }
