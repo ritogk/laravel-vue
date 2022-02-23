fd04999b9b6ad945501d75678e4d5bfab3abfab7
r.ito <homing0321r4cfw@yahoo.co.jp>
2022-02-23 15:22:44 +0900

    ts generator 追加

---
+++ open_api_generator/generator_ts.sh
@@ -0,0 +1,13 @@
+WORK_PATH='/out/ts'
+# コード生成
+docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v5.3.0 generate -i /local/api.yaml -g typescript-fetch -o /local${WORK_PATH} -c /local/config_ts.json
+
+
+# OUTPUT_PATH='../project/app/OpenAPI'
+# laravel側にコピー
+# mkdir -p ${OUTPUT_PATH}
+# cp -r .${WORK_PATH}/lib/* ${OUTPUT_PATH}
+# cp api.yaml ${OUTPUT_PATH}
+ 
+#作業ディレクトリ削除
+# rm -rf ./out
