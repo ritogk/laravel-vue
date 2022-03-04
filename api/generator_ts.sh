565207e045f5efe35d861d3b9b6f7422d8c80c28
root <homing-job@yahoo.co.jp>
2022-03-04 19:07:21 +0900

    ディレクトリ変更

--- api/generator_ts.sh
+++ api/generator_ts.sh
@@ -2,7 +2,7 @@ WORK_PATH='/out/ts'
 # コード生成
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v5.3.0 generate -i /local/api.yaml -g typescript-fetch -o /local${WORK_PATH} -c /local/config_ts.json
 
-OUTPUT_PATH='../front/project/src/open_api'
+OUTPUT_PATH='../front/src/open_api'
 # vue側にコピー
 mkdir -p ${OUTPUT_PATH}
 cp -r .${WORK_PATH}/* ${OUTPUT_PATH}
