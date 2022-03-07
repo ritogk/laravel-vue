6933894648233963b66ac35bc39ad09234f0e081
root <homing-job@yahoo.co.jp>
2022-03-08 08:35:52 +0900

    詳細画面のコンポーネント作成

--- front/project/src/components/JobEntry/ListBox.vue
+++ front/project/src/components/JobEntry/ListBox.vue
@@ -74,52 +74,7 @@
     >
       <div class="modal-dialog modal-lg">
         <div class="modal-content">
-          <div class="modal-header bg-primary">
-            <h5 class="modal-title text-white" id="modalLabel">
-              プログラマー募集!!
-            </h5>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body">
-            <div class="card">
-              <img
-                :src="`https://business-textbooks.com/btextbooks/wp-content/uploads/2020/02/3854adc57415272a6dbdaf4017e4d6b4-135.jpg`"
-                class="card-img-top"
-              />
-              <div class="card-body">
-                <div>
-                  <h1>プログラマー募集!!</h1>
-                </div>
-                <div class="py-3">
-                  <h3>仕事内容</h3>
-                  <span class="multiple"></span>
-                </div>
-                <div class="py-3">
-                  <h3>金額</h3>
-                  <span></span>
-                </div>
-                <div class="py-3">
-                  <h3>福利厚生</h3>
-                  <span class="multiple"></span>
-                </div>
-                <div class="py-3">
-                  <h3>休日</h3>
-                  <span class="multiple"></span>
-                </div>
-
-                <div class="py-2">
-                  <button type="button" class="btn btn-primary w-100">
-                    申し込む
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
+          <DetailBox />
         </div>
       </div>
     </div>
@@ -128,8 +83,11 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import 'bootstrap';
+import DetailBox from '@/components/JobEntry/DetailBox.vue';
 export default defineComponent({
+  components: {
+    DetailBox,
+  },
   setup() {
     // const click_job = () => {
     //   new bootstrap.Modal(document.getElementById('myModal'), {
