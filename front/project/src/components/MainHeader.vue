893fe36cd24b9de521c605b5341ec9788685b4d8
root <homing-job@yahoo.co.jp>
2022-03-13 15:41:43 +0900

    ヘッダーのリンク追加

--- front/project/src/components/MainHeader.vue
+++ front/project/src/components/MainHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">求人検索</a>
+      <a class="navbar-brand" href="#" @click="clickTitle">求人検索</a>
       <button
         class="navbar-toggler"
         type="button"
@@ -24,7 +24,10 @@
             <a class="nav-link" href="#">ユーザー名</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">ログイン</a>
+            <a class="nav-link" href="#" @click="clickLogin">ログイン</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" @click="clickRegistration">新規登録</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">管理画面</a>
@@ -36,14 +39,25 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
 
-@Options({
-  props: {
-    msg: String,
+export default defineComponent({
+  setup() {
+    const router = useRouter();
+    // 「タイトル」押下
+    const clickTitle = () => {
+      router.push('/');
+    };
+    // 「新規登録」押下
+    const clickRegistration = () => {
+      router.push({ name: 'UserRegistration' });
+    };
+    // 「ログイン」押下
+    const clickLogin = () => {
+      router.push({ name: 'UserLogin' });
+    };
+    return { clickTitle, clickRegistration, clickLogin };
   },
-})
-export default class HelloWorld extends Vue {
-  msg!: string;
-}
+});
 </script>
