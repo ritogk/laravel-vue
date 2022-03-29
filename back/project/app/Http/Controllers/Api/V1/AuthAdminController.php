09d2bd2810ce5833a8c428d9fad3962705cc8fbe
root <homing-job@yahoo.co.jp>
2022-03-29 17:44:43 +0900

    コントローラーをapiのバージョンに合わせて仕分け

--- back/project/app/Http/Controllers/Api/V1/AuthAdminController.php
+++ back/project/app/Http/Controllers/Api/V1/AuthAdminController.php
@@ -1,20 +1,19 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 use \Cookie;
+use App\Libs\Consts;
 // request
-use App\Http\Requests\Auth\Front\LoginRequest;
+use App\Http\Requests\Auth\Admin\LoginRequest;
 // openapi
 use App\OpenAPI;
 use App\Libs\OpenAPIUtility;
 
-use App\Libs\Consts;
-
-class AuthFrontController extends Controller
+class AuthAdminController extends Controller
 {
     /**
      * login
@@ -26,17 +25,18 @@ class AuthFrontController extends Controller
     {
         $requestBody = new OpenAPI\Model\RequestLogin($request->all());
         $request_all = ['email' => $requestBody->getEmail(), 'password' => $requestBody->getPassword()];
-        if (!$token = auth('user')->attempt($request_all)) {
+        if (!$token = auth('admin')->attempt($request_all)) {
             return response()->json(
                 ['errors' => ['message' => ['認証に失敗しました。']]],
                 Response::HTTP_UNAUTHORIZED
             );
         }
+
         $result = $this->respondWithToken($token);
         $response_model = OpenAPIUtility::dicstionaryToModelContainer(OpenAPI\Model\AccessToken::class, $result);
-        $cookie = cookie(Consts::coockie_nm_dict['USER_JWT'], $response_model->accessToken, $response_model->expiresIn);
+        $cookie = cookie(Consts::coockie_nm_dict['ADMIN_JWT'], $response_model->accessToken, $response_model->expiresIn);
         return response()->json(
-            $response_model,
+            OpenAPIUtility::dicstionaryToModelContainer(OpenAPI\Model\AccessToken::class, $result),
             Response::HTTP_CREATED
         )->cookie($cookie);
     }
@@ -49,7 +49,7 @@ class AuthFrontController extends Controller
     public function me(): JsonResponse
     {
         return response()->json(
-            OpenAPIUtility::dicstionaryToModelContainer(OpenAPI\Model\User::class, auth('user')->user()->toArray()),
+            OpenAPIUtility::dicstionaryToModelContainer(OpenAPI\Model\Admin::class, auth('admin')->user()->toArray()),
             Response::HTTP_OK
         );
     }
@@ -61,11 +61,11 @@ class AuthFrontController extends Controller
      */
     public function logout(): JsonResponse
     {
-        auth('user')->logout();
+        auth('admin')->logout();
         return response()->json(
             ['message' => 'Successfully logged out'],
             Response::HTTP_NO_CONTENT
-        )->cookie(Cookie::forget(Consts::coockie_nm_dict['USER_JWT']));
+        )->cookie(Cookie::forget(Consts::coockie_nm_dict['ADMIN_JWT']));
     }
 
     /**
@@ -75,11 +75,11 @@ class AuthFrontController extends Controller
      */
     public function refresh(): JsonResponse
     {
-        $result = $this->respondWithToken(auth('user')->refresh());
+        $result = $this->respondWithToken(auth('admin')->refresh());
         $response_model = OpenAPIUtility::dicstionaryToModelContainer(OpenAPI\Model\AccessToken::class, $result);
-        $cookie = cookie(Consts::coockie_nm_dict['USER_JWT'], $response_model->accessToken, $response_model->expiresIn);
+        $cookie = cookie(Consts::coockie_nm_dict['ADMIN_JWT'], $response_model->accessToken, $response_model->expiresIn);
         return response()->json(
-            $response_model,
+            OpenAPIUtility::dicstionaryToModelContainer(OpenAPI\Model\AccessToken::class, $result),
             Response::HTTP_CREATED
         )->cookie($cookie);
     }
@@ -97,7 +97,7 @@ class AuthFrontController extends Controller
         return [
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth('user')->factory()->getTTL() * 60
+            'expires_in' => auth('admin')->factory()->getTTL() * 60
         ];
     }
 }
--- back/project/routes/api_v1.php
+++ back/project/routes/api_v1.php
@@ -14,51 +14,51 @@ use App\Http\Controllers;
 Route::group(['prefix' => 'auth'], function () {
     // 管理者
     Route::group(['prefix' => 'admin'], function () {
-        Route::get('/me', [Controllers\Api\AuthAdminController::class, 'me'])->middleware(['auth:admin']);
-        Route::post('/login', [Controllers\Api\AuthAdminController::class, 'login']);
-        Route::post('/refresh', [Controllers\Api\AuthAdminController::class, 'refresh']);
-        Route::post('/logout', [Controllers\Api\AuthAdminController::class, 'logout'])->middleware(['auth:admin']);
+        Route::get('/me', [Controllers\Api\V1\AuthAdminController::class, 'me'])->middleware(['auth:admin']);
+        Route::post('/login', [Controllers\Api\V1\AuthAdminController::class, 'login']);
+        Route::post('/refresh', [Controllers\Api\V1\AuthAdminController::class, 'refresh']);
+        Route::post('/logout', [Controllers\Api\V1\AuthAdminController::class, 'logout'])->middleware(['auth:admin']);
     });
     // 一般
     Route::group(['prefix' => 'front'], function () {
-        Route::get('/me', [Controllers\Api\AuthFrontController::class, 'me'])->middleware(['auth:user']);
-        Route::post('/login', [Controllers\Api\AuthFrontController::class, 'login']);
-        Route::post('/logout', [Controllers\Api\AuthFrontController::class, 'logout'])->middleware(['auth:user']);
-        Route::post('/refresh', [Controllers\Api\AuthFrontController::class, 'refresh']);
+        Route::get('/me', [Controllers\Api\V1\AuthFrontController::class, 'me'])->middleware(['auth:user']);
+        Route::post('/login', [Controllers\Api\V1\AuthFrontController::class, 'login']);
+        Route::post('/logout', [Controllers\Api\V1\AuthFrontController::class, 'logout'])->middleware(['auth:user']);
+        Route::post('/refresh', [Controllers\Api\V1\AuthFrontController::class, 'refresh']);
     });
 });
 
 // 仕事マスタ
 Route::group(['prefix' => 'jobs'], function () {
-    Route::get('/', [Controllers\Api\Master\JobController::class, 'list']);
-    Route::get('/{id}', [Controllers\Api\Master\JobController::class, 'find']);
-    Route::post('/', [Controllers\Api\Master\JobController::class, 'create'])->middleware(['auth:admin']);
-    Route::put('/{id}', [Controllers\Api\Master\JobController::class, 'update'])->middleware(['auth:admin']);
-    Route::delete('/{id}', [Controllers\Api\Master\JobController::class, 'destroy'])->middleware(['auth:admin']);
+    Route::get('/', [Controllers\Api\V1\JobController::class, 'list']);
+    Route::get('/{id}', [Controllers\Api\V1\JobController::class, 'find']);
+    Route::post('/', [Controllers\Api\V1\JobController::class, 'create'])->middleware(['auth:admin']);
+    Route::put('/{id}', [Controllers\Api\V1\JobController::class, 'update'])->middleware(['auth:admin']);
+    Route::delete('/{id}', [Controllers\Api\V1\JobController::class, 'destroy'])->middleware(['auth:admin']);
 });
 
 // 仕事カテゴリ マスタ
 Route::group(['prefix' => 'job_categories'], function () {
-    Route::get('/', [Controllers\Api\Master\JobCategoryController::class, 'list']);
-    Route::get('/{id}', [Controllers\Api\Master\JobCategoryController::class, 'find']);
-    Route::post('/', [Controllers\Api\Master\JobCategoryController::class, 'create'])->middleware(['auth:admin']);
-    Route::put('/{id}', [Controllers\Api\Master\JobCategoryController::class, 'update'])->middleware(['auth:admin']);
-    Route::delete('/{id}', [Controllers\Api\Master\JobCategoryController::class, 'destroy'])->middleware(['auth:admin']);
+    Route::get('/', [Controllers\Api\V1\JobCategoryController::class, 'list']);
+    Route::get('/{id}', [Controllers\Api\V1\JobCategoryController::class, 'find']);
+    Route::post('/', [Controllers\Api\V1\JobCategoryController::class, 'create'])->middleware(['auth:admin']);
+    Route::put('/{id}', [Controllers\Api\V1\JobCategoryController::class, 'update'])->middleware(['auth:admin']);
+    Route::delete('/{id}', [Controllers\Api\V1\JobCategoryController::class, 'destroy'])->middleware(['auth:admin']);
 });
 
 // エントリー マスタ
 Route::group(['prefix' => 'entries'], function () {
-    Route::get('/', [Controllers\Api\Master\EntryController::class, 'list'])->middleware(['auth:admin']);
-    Route::post('/', [Controllers\Api\Master\EntryController::class, 'create'])->middleware(['auth:user']);
+    Route::get('/', [Controllers\Api\V1\EntryController::class, 'list'])->middleware(['auth:admin']);
+    Route::post('/', [Controllers\Api\V1\EntryController::class, 'create'])->middleware(['auth:user']);
 });
 
 // 会員マスタ
 Route::group(['prefix' => 'users'], function () {
-    Route::get('/', [Controllers\Api\Master\UserController::class, 'list'])->middleware(['auth:admin']);
-    Route::post('/', [Controllers\Api\Master\UserController::class, 'create']);
+    Route::get('/', [Controllers\Api\V1\UserController::class, 'list'])->middleware(['auth:admin']);
+    Route::post('/', [Controllers\Api\V1\UserController::class, 'create']);
 });
 
 // ファイル操作系
 Route::group(['prefix' => 'files'], function () {
-    Route::post('/', [Controllers\Api\FileController::class, 'upload'])->middleware(['auth:admin']);
+    Route::post('/', [Controllers\Api\V1\FileController::class, 'upload'])->middleware(['auth:admin']);
 });
