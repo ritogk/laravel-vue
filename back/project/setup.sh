6777bbf39f04b48039acff8c76fa68d0776bb79a
root <homing-job@yahoo.co.jp>
2022-04-13 19:42:58 +0900

    change

--- back/project/setup.sh
+++ back/project/setup.sh
@@ -8,7 +8,7 @@ php artisan storage:link
 wget https://github.com/homing-job/laravel-spa/files/6542800/seeder_images.zip
 unzip seeder_images.zip -d storage/app/public/images
 rm -f seeder_images.zip
-php artisan jwt:secret
+yes | php artisan jwt:secret
 
 php artisan key:generate --env=testing
-php artisan jwt:secret --env=testing
+yes | php artisan jwt:secret --env=testing
