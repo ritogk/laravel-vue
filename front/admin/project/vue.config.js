45c4659ffe329980778ee786f235fb7244afe4a3
root <homing-job@yahoo.co.jp>
2022-04-14 12:59:22 +0900

    ページタイトル変更

--- front/admin/project/vue.config.js
+++ front/admin/project/vue.config.js
@@ -6,4 +6,10 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   // キャッシュバスティングのためにファイル名にハッシュをつけている。
   filenameHashing: true,
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+      title: '管理画面',
+    },
+  },
 };
